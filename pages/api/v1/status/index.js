3b8cdde59ef72f4dfb474035c7eb841f531c2b67
Paulo Henrique Viana <53318055+Phviana@users.noreply.github.com>
2024-02-07 18:00:39 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from '../../../../infra/database.js'
+
+async function status(request, response) {
+  const queryResut = await database.query("SELECT 1 + 1;")
+  console.log(queryResut.rows)
   response.status(200).json({ valor: "status OK" });
 }
 
