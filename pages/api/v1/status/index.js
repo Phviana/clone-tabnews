4c4be3cdfad41adf628753b428f297a450b8ef04
Paulo Henrique Viana <53318055+Phviana@users.noreply.github.com>
2024-05-02 12:57:34 +0000

    alteraçao na pagina de status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,30 @@
 import database from 'infra/database.js'
 
 async function status(request, response) {
-  const queryResut = await database.query("SELECT 1 + 1;")
-  console.log(queryResut.rows)
-  response.status(200).json({ valor: "status OK" });
+  const updatedAt = new Date().toISOString()
+
+  const databaseResult = await database.query("SHOW server_version;")
+  const databaseVersion = databaseResult.rows[0].server_version
+
+  const databaseResultMaxConnections = await database.query("SHOW max_connections;")
+  const max_connections = databaseResultMaxConnections.rows[0].max_connections
+
+  const databaseResultOpenedConnections = await database.query({text: "SELECT Count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [process.env.POSTGRES_DB]
+  }
+  )
+  const openedConnections = databaseResultOpenedConnections.rows[0].count
+  
+  response.status(200).json({ 
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(max_connections),
+        opened_connections: openedConnections
+      }
+    }
+   });
 }
 
 export default status;
